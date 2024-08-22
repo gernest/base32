4dd20a8b6f9561d80e98fc0f88b35aa461bf019a
John Benediktsson <mrjbq7@gmail.com>
2024-08-22 14:43:39 -0700

    Fix example.zig for zig 0.13.

--- example.zig
+++ example.zig
@@ -10,16 +10,16 @@ pub fn main() !void {
 
 fn encodeString() !void {
     const src = "any + old & data";
-    comptime const size = base32.std_encoding.encodeLen(src.len);
+    const size = comptime base32.std_encoding.encodeLen(src.len);
     var buf: [size]u8 = undefined;
-    var out = base32.std_encoding.encode(&buf, src);
+    const out = base32.std_encoding.encode(&buf, src);
     try stdout.print("ExampleEncodingToString:\nsource: \"{s}\"\noutput: {s}\n", .{ src, out });
 }
 
 fn decodeString() !void {
     const src = "ONXW2ZJAMRQXIYJAO5UXI2BAAAQGC3TEEDX3XPY=";
-    comptime const size = base32.std_encoding.decodeLen(src.len);
+    const size = comptime base32.std_encoding.decodeLen(src.len);
     var buf: [size]u8 = undefined;
-    var out = try base32.std_encoding.decode(&buf, src);
+    const out = try base32.std_encoding.decode(&buf, src);
     try stdout.print("ExampleDecodingString:\nsource: {s}\noutput: \"{s}\"\n", .{ src, out });
 }
